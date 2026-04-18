cf95473cdcae3e52b54562bc6b4f8e0dbb2e4c2f
Nikhila Reddy <2420030387@klh.edu.in>
2026-04-18 14:56:21 +0530

    Week 3 - Hibernate HQL Project

---
+++ week3-hibernate-hql/src/main/java/com/example/entity/Product.java
@@ -0,0 +1,52 @@
+package com.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    // Mandatory no-arg constructor
+    public Product() {}
+
+    public Product(String name, String description, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
+
